29a51472b5383d83bbda838e72dbf0f33ae07b67
douglas <douglasfortunato@hotmail.com>
2020-03-30 12:09:58 -0300

    Fix Port

--- src/index.js
+++ src/index.js
@@ -3,8 +3,10 @@ const cors = require('cors')
 const routes = require('./routes')
 const app = express()
 
+const port = process.env.PORT || 3333
+
 app.use(cors())
 app.use(express.json())
 app.use(routes)
 
-app.listen(3333)
+app.listen(port)
